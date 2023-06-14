9fb4c606ff6b94a0d328c95c854db68d5e1dbfc6
Mariana Correa <mpcs270391@gmail.com>
2023-06-14 19:03:48 -0300

    fix: Moves header to a separated component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Header extends Component {
   render() {
+    const { email } = this.props;
+
     return (
-      <div>Header</div>
+      <header>
+        <h1>Wallet</h1>
+        <span data-testid="email-field">{email}</span>
+        <span data-testid="total-field">Despesas Totais: 0</span>
+        <span data-testid="header-currency-field">Câmbio: BRL</span>
+      </header>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = ({ user, wallet }) => ({
+  email: user.email,
+  totalExpenses: wallet.totalExpenses,
+  cambio: wallet.currencies,
+});
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,29 +1,12 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import Header from '../components/Header';
 
 class Wallet extends React.Component {
   render() {
-    const { email } = this.props;
     return (
-      <header>
-        <h1>Wallet</h1>
-        <span data-testid="email-field">{email}</span>
-        <span data-testid="total-field">Despesas Totais: 0</span>
-        <span data-testid="header-currency-field">Câmbio: BRL</span>
-      </header>
+      <Header />
     );
   }
 }
 
-const mapStateToProps = ({ user, wallet }) => ({
-  email: user.email,
-  totalExpenses: wallet.totalExpenses,
-  cambio: wallet.currencies,
-});
-
-Wallet.propTypes = {
-  email: PropTypes.string.isRequired,
-};
-
-export default connect(mapStateToProps)(Wallet);
+export default Wallet;
