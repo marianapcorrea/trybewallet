816bd5da6bad5dcb32f2290e30d5e80a1bf6b960
Mariana Correa <mpcs270391@gmail.com>
2023-06-17 22:57:37 -0300

    feat: requirement 6 - Creates table's headers

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,7 +3,27 @@ import React, { Component } from 'react';
 class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <table>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>The table body</td>
+            <td>with two columns</td>
+          </tr>
+        </tbody>
+      </table>
     );
   }
 }
